7cc6c596dd4438cda5dee9c3bf36fc58934f82bd
Carl Fluke <carl.fluke@coinbase.com>
2022-05-26 12:52:25 -0400

    initial commit

---
+++ apps/cb-pay-demo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'cb-pay-demo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/cb-pay-demo',
+};
